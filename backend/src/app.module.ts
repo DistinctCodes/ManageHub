abe03481fce5794a11dc089333a892f57e82a5b8
peteroche <petergoddey08@gmail.com>
2025-07-25 09:39:33 +0100

    feat: leave request

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BadgesModule } from './badges/badges.module';
 import { PollsModule } from './polls/polls.module';
+import { LeaveModule } from './leave/leave.module';
 
 @Module({
-  imports: [BadgesModule, PollsModule],
+  imports: [BadgesModule, PollsModule, LeaveModule],
   controllers: [AppController],
   providers: [AppService],
 })
