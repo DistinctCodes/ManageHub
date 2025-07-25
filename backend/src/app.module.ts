a2a4357360883fcd30d5458384e265bf142fce3e
nafiuishaaq <nafiuig@gmail.com>
2025-07-25 14:51:47 +0100

    implemented the bio-sync

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,12 @@ import { BadgesModule } from './badges/badges.module';
 import { InternetSpeedModule } from './internet-speed/internet-speed.module';
 import { LibraryService } from './library/library.service';
 import { LibraryModule } from './library/library-module.module';
+import { BiometricSyncModule } from './biometric-sync/biometric-sync.module';
+import { PollsModule } from './polls/polls.module';
+import { LeaveModule } from './leave/leave.module';
 
 @Module({
-  imports: [BadgesModule, InternetSpeedModule, LibraryModule, PollsModule, LeaveModule],
+  imports: [BadgesModule, InternetSpeedModule, LibraryModule, PollsModule, LeaveModule, BiometricSyncModule],
   controllers: [AppController],
   providers: [AppService, LibraryService],
 })
