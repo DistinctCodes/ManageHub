025edf290848661f696c6c17857b111c8cd547ab
peteroche <petergoddey08@gmail.com>
2025-07-25 09:33:39 +0100

    feat: yes/no polls

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BadgesModule } from './badges/badges.module';
+import { PollsModule } from './polls/polls.module';
 
 @Module({
-  imports: [BadgesModule],
+  imports: [BadgesModule, PollsModule],
   controllers: [AppController],
   providers: [AppService],
 })
