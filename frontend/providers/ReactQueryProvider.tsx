14fdb8d65187f804e45e986dff2d13a2148dd1ac
Ugochukwu Nebolisa <nebolisaugochukwu@gmail.com>
2025-09-25 23:15:59 +0100

    chore: initialized the queryClient using the useState hook

--- frontend/providers/ReactQueryProvider.tsx
+++ frontend/providers/ReactQueryProvider.tsx
@@ -1,13 +1,16 @@
+"use client";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-const queryClient = new QueryClient();
+import { useState } from "react";
 
 export default function ReactQueryProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
