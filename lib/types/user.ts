c5b55c514a3e4ac747b55a557537c84cedc9ba32
akronim26 <31AbhivanshJ@gmail.com>
2025-09-25 21:46:50 +0530

    feat: add user and auth type definitions

---
+++ lib/types/user.ts
@@ -0,0 +1,43 @@
+export enum UserRole {
+  USER = "user",
+  ADMIN = "admin",
+}
+
+export interface User {
+  id: string;
+  firstname: string;
+  lastname: string;
+  username?: string | null;
+  email: string;
+  role: UserRole;
+  isActive: boolean;
+  isSuspended: boolean;
+  isDeleted: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+}
+
+export interface RegisterUser {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+}
+
+export interface LoginUser {
+  email: string;
+  password: string;
+}
+
+export interface AuthState {
+  user: User | null;
+  accessToken: string | null;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+}
+
+export interface AuthResponse {
+  user: User;
+  accessToken: string;
+}
