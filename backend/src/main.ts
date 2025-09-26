ef3f61e86e7e0e81576dc36642ccbbe75ab901c0
Yusuf Falade <faladeyusuf54@gmail.com>
2025-09-26 15:08:48 +0100

    added the validation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //GLOBAL VALIDATION
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   //ENABLE CORS
   app.enableCors({
