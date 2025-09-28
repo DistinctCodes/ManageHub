245d7b939156703674a1a3466bbadd618efc4f85
mftee <falademaryam78@gmail.com>
2025-09-28 14:15:41 +0100

    providers created and fixed the bugs

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { RefreshToken } from './entities/refreshToken.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './providers/auth.service';
 import { UsersModule } from '../users/users.module';
+import { HashingProvider } from './providers/hashing.provider';
+import { BcryptProvider } from './providers/bcrypt.provider';
 
 @Module({
   imports: [
