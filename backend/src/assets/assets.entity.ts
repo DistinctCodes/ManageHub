7788b8801c9db2a3c65311b86a18adbe29b1eb28
Sandijigs <idjigheresandra@gmail.com>
2025-10-04 21:56:57 +0100

    Implemented Logout and Logout-All Sessions Endpoints

--- backend/src/assets/assets.entity.ts
+++ backend/src/assets/assets.entity.ts
@@ -1,5 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
 import { Supplier } from '../suppliers/suppliers.entity';
+import { AssetMaintenance } from '../asset-maintenance/asset-maintenence.entity';
 
 @Entity('assets')
 export class Asset {
@@ -14,4 +15,7 @@ export class Asset {
 
   @ManyToOne(() => Supplier, supplier => supplier.assets)
   supplier: Supplier;
+
+  @OneToMany(() => AssetMaintenance, (maintenance) => maintenance.asset)
+  maintenances: AssetMaintenance[];
 }
