38a9cf0564953552aecb2b589a79387037a90e14
marinad <marinaordukhanyan3@gmail.com>
2026-03-18 23:11:38 +0400

    feat: Frontend: Payment TypeScript Type Definitions

---
+++ frontend/lib/types/payment.ts
@@ -0,0 +1,23 @@
+// import { Booking } from "@/lib/types/booking"
+
+export type PaymentStatus = "PENDING" | "SUCCESS" | "FAILED"
+
+export interface Payment {
+id: string
+reference: string
+amountKobo: number
+status: PaymentStatus
+paidAt: string | null
+userId: string
+bookingId: string
+booking?: Booking 
+user?: { id: string; firstname: string; lastname: string; email: string }
+createdAt: string
+updatedAt: string
+}
+
+export interface InitiatePaymentResponse {
+    authorizationUrl: string
+    reference: string
+}
+
