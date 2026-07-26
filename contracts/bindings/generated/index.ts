f359e732a59f3a6064074255724e155b2dec87e5
bigben-7 <benardsimon7@gmail.com>
2026-07-26 20:12:26 +0100

    feat: add TS bindings generator, credit expiry/reconciliation, threat model, and pin Rust toolchain
    Closes #1540, #1541, #1542, #1543

---
+++ contracts/bindings/generated/index.ts
@@ -0,0 +1,7 @@
+// Re-export all generated contract bindings
+export * from "./manage_hub";
+export * from "./access_control";
+export * from "./membership_token";
+export * from "./workspace_booking";
+export * from "./payment_escrow";
+export * from "./resource_credits";
